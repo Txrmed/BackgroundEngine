f8dfa747d0657f56a7871a5bd8f24a84a89710c9
Mateusz <mateusz68latos@gmail.com>
2020-08-02 13:07:12 +0200

    final commit

--- engine.py
+++ engine.py
@@ -10,31 +10,29 @@ urls = []
 
 def generate_urls():
     if len(urls) == 0:
-        for post in r.subreddit('Wallpapers').top():
+        for post in r.subreddit("Wallpaper").top():
             url = post.url
             if url[-1] == 'g' and url[-2] == 'p' and url[-3] == 'j' and url[-4] == '.':
                 urls.append(url)
+            print(url)
 
 
 generate_urls()
 
-download = None
-index = len(urls)
-
-random = random.randint(0, index)
+random = random.randint(0, len(urls))
 post = urls[random]
 download = requests.get(post, allow_redirects=False)
 print(" Post URL {}".format(post))
 
 urls.pop(random)
-index -= 1
 
 open('X:\\Code\\Projects\\BackgroundEngine\\download.jpg', 'wb').write(download.content)
 
 ctypes.windll.user32.SystemParametersInfoW(20, 0, "X:\\Code\\Projects\\BackgroundEngine\\download.jpg" , 0)
 print(" Set wallpaper")
 
-time.sleep(5)
+
+time.sleep(1)
 os.remove("X:\\Code\\Projects\\BackgroundEngine\\download.jpg")
 print(" Removed download.jpg")
 time.sleep(2)
