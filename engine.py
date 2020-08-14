dc39fbf2af442795a2c871ee9dd112e4631772d5
Mateusz <mateusz68latos@gmail.com>
2020-08-14 22:39:55 +0200

    added a logging system to log urls used for wallpapers

--- engine.py
+++ engine.py
@@ -4,36 +4,36 @@ import time
 import random
 import os
 import ctypes
+from dotenv import load_dotenv
+import time
 
 r = praw.Reddit('bot2')
 urls = []
 
 def generate_urls():
     if len(urls) == 0:
-        for post in r.subreddit("Wallpaper").top():
+        for post in r.subreddit("Wallpaper").top() and r.subreddit("Wallpaper").hot():
             url = post.url
             if url[-1] == 'g' and url[-2] == 'p' and url[-3] == 'j' and url[-4] == '.':
                 urls.append(url)
-            print(url)
 
 
 generate_urls()
 
 random = random.randint(0, len(urls))
 post = urls[random]
-download = requests.get(post, allow_redirects=False)
-print(" Post URL {}".format(post))
 
-urls.pop(random)
+download = requests.get(post, allow_redirects=False)
 
 open('X:\\Code\\Projects\\BackgroundEngine\\download.jpg', 'wb').write(download.content)
 
 ctypes.windll.user32.SystemParametersInfoW(20, 0, "X:\\Code\\Projects\\BackgroundEngine\\download.jpg" , 0)
-print(" Set wallpaper")
 
+with open("logs.log", "a") as f:
+    current_unix = time.time()
+    f.write("\n[{}] Set Wallpaper to {}".format(current_unix, post))
 
-time.sleep(1)
-os.remove("X:\\Code\\Projects\\BackgroundEngine\\download.jpg")
-print(" Removed download.jpg")
 time.sleep(2)
+
+os.remove("X:\\Code\\Projects\\BackgroundEngine\\download.jpg")
 exit()
