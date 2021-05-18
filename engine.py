b70d34db78001e156fac123532723736c05ee88c
Mateusz <mateusz68latos@gmail.com>
2021-05-18 14:17:56 +0200

    Added a debug.json file for easier debugging. Added a check to not set the same image as wallpaper twice

--- engine.py
+++ engine.py
@@ -4,14 +4,14 @@ import random
 import os
 import ctypes
 import time
-import threading
+import json
 
 time_begin = time.time()
 
 path = "X:\\Code\\Projects\\BackgroundEngine\\downloads\\"
 util_path = "X:\\Code\\Projects\\BackgroundEngine\\"
 
-r = praw.Reddit('bot2')
+r = praw.Reddit('bot1')
 urls = []
 
 
@@ -25,6 +25,7 @@ def generateUrls():
 
     if len(urls) == 0:
         generateUrls()
+		
     gen_e = time.time()
 
 def removeDownloadsFolderContents(rule):
@@ -35,25 +36,46 @@ def removeDownloadsFolderContents(rule):
                 os.remove(path + file)
             except:
                 pass
-            
+			
+def debug(download_speed, urls, post):
+	with open('debug.json', "r") as f:
+		data = json.load(f)
+		if data['debug'] is False:
+			return
+		if data["download_speed"] is True:
+			print("Download Speed : {} MB/s,".format(download_speed / 1000000))
+		if data["wallpaper_list_length"] is True:
+			print("Wallpaper List Length: {}".format(len(urls)))
+		if data["wallpaper_url"] is True:
+			print("Wallpaper set to: {}".format(post))
+
 def setImgWallpaper():
 
-    generateUrls()
-    print(len(urls))
-    post = urls[random.randint(0, len(urls) - 1)]
-    print(post)
+	generateUrls()
+	index = random.randint(0, len(urls) - 1)
+	post = urls[index]
 
-    unix_time = time.time()
-
-    d_time_s = time.time()
-    open(path + "{}.jpg".format(int(unix_time)), 'wb').write(requests.get(post, allow_redirects=False).content)
-
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, path + "{}.jpg".format(int(unix_time)) , 0)
-
-    removeDownloadsFolderContents(10)
-    
-    with open(util_path + "time.txt", "a") as f:
-        f.write("\n{} : {} : {}".format(time.time() - time_begin, d_time_o, gen_e - gen))
-
-    exit()
+	with open("used.txt", "r+") as f:
+		lines = f.read().splitlines()
+		for i in range(len(lines)):
+			if post == lines[i]:
+				urls.pop(index)
+				post = urls[random.randint(0, len(urls) - 1)]
+		f.write("{}\n".format(post))
+	
+	unix_time = time.time()
+	
+	download_start = time.time()
+	open(path + "{}.jpg".format(int(unix_time)), 'wb').write(requests.get(post, allow_redirects=False).content)
+	download_ready = time.time()
+	
+	size = requests.get(post, stream=True).headers['Content-length']
+	
+	download_speed = float(size) / (download_ready - download_start)
+	
+	ctypes.windll.user32.SystemParametersInfoW(20, 0, path + "{}.jpg".format(int(unix_time)) , 0)
+	removeDownloadsFolderContents(10)
+	debug(download_speed, urls, post)
+	exit()
+	
 setImgWallpaper()
